eafa24007e00ead1a21b7c169789018ae7efdb77
Joao Pedro Campos Silva <joaopedrocampossilva97@gmail.com>
2020-11-14 21:19:52 -0300

    Changing files to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import * as React from 'react';
-import { render } from 'react-dom';
 import { Admin, Resource, ListGuesser, DataProvider } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 import { WorldList, WorldEdit, WorldCreate, WorldIcon } from './worlds';
 
 const dataProvider: DataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const App = () =>
-    render(
-        <Admin dataProvider={dataProvider}>
-            <Resource
-                name="world"
-                list={WorldList}
-                edit={WorldEdit}
-                create={WorldCreate}
-                icon={WorldIcon}
-            />
-            <Resource name="users" list={ListGuesser} />
-        </Admin>,
-        document.getElementById('root')
-    );
+const App = () => {
+    return <Admin dataProvider={dataProvider}>
+        <Resource
+            name="world"
+            list={WorldList}
+            edit={WorldEdit}
+            create={WorldCreate}
+            icon={WorldIcon}
+        />
+        <Resource name="users" list={ListGuesser} />
+    </Admin>
+};
 
 export default App;
